7dd6cc5f9448ee6be442b480e99923c922babb4c
ArdaWiraz <73760249+ArdaWiraz@users.noreply.github.com>
2021-03-28 19:43:37 +0400

    release version1

---
+++ main/forms.py
@@ -0,0 +1,13 @@
+from .models import Task
+from django.forms import ModelForm, TextInput, Textarea
+
+class TaskForm(ModelForm):
+	class Meta:
+		model = Task
+		fields = ["title", "task"]
+		widgets ={
+			"title": TextInput(attrs = {'class': 'form-control', 'placeholder': 'type the name'}),
+			"task": Textarea(attrs = {'class': 'form-control', 'placeholder': 'type the description'})
+
+
+		}
